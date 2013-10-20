0de9786e44a3c36394c089d7c47bb4f346d8282c
John Roll <john@cfa.harvard.edu>
2013-10-20 12:30:51 -0400

    UI Building

--- template.js
+++ template.js
@@ -1,44 +1,50 @@
-	function template(templateid,data){
+function template(templateid,data){
+    
+    var el = document.getElementById(templateid);
+
+    if ( el ) {
+	return el.innerHTML.replace(/{([a-zA-Z0-9_.%]*)}/g,
+	    function(m,key){
+		var type, prec, val;
+		var val = data;
 	    
-	    return document.getElementById(templateid).innerHTML.replace(/{([a-zA-Z0-9_.%]*)}/g,
-		function(m,key){
-		    var type, prec, val;
-		    var val = data;
-		
-		    key = key.split("%");
+		key = key.split("%");
 
-		    if ( key.length <= 1 ) {
-			fmt = "%s"
-		    } else {
-			fmt = key[1]
-		    }
-
-		    key = key[0]
-		    key = key.split(".");
-
-		    for ( i = 0; i < key.length; i++ ) {
-			if ( val.hasOwnProperty(key[i]) ) {
-			    val = val[key[i]];
-			} else {
-			    return "";
-			}
-		    }
-
-		    type = fmt.substring(fmt.length-1)
-		    prec = fmt.substring(1, fmt.length-1)
-
-		    switch ( type ) {
-		     case "s":
-			break;
-		     case "f":
-			val = val.toFixed(prec);
-			break;
-		     case "d":
-			val = val.toFixed(0);
-			break;
-		    }
-
-		    return val;
+		if ( key.length <= 1 ) {
+		    fmt = "%s"
+		} else {
+		    fmt = key[1]
 		}
-	    );
-	}
+
+		key = key[0]
+		key = key.split(".");
+
+		for ( i = 0; i < key.length; i++ ) {
+		    if ( val.hasOwnProperty(key[i]) ) {
+			val = val[key[i]];
+		    } else {
+			return "";
+		    }
+		}
+
+		type = fmt.substring(fmt.length-1)
+		prec = fmt.substring(1, fmt.length-1)
+
+		switch ( type ) {
+		 case "s":
+		    break;
+		 case "f":
+		    val = val.toFixed(prec);
+		    break;
+		 case "d":
+		    val = val.toFixed(0);
+		    break;
+		}
+
+		return val;
+	    }
+	);
+    } else {
+	return "";
+    }
+}
