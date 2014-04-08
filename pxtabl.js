18ebfaf4a767846da15c8044116f15be1765ac92
John Roll <john@cfa.harvard.edu>
2014-04-07 20:49:56 -0400

    pixel table mostly working

--- pxtabl.js
+++ pxtabl.js
@@ -19,22 +19,26 @@
     });
  */
 
-    function strrep(x, n) {
+    function strrep(str, n, x) {
+        var i;
+	var rx = RegExp(x, "g");
+
 	var s = '';
-	for (;;) {
-	    if (n & 1) s += x;
-	    n >>= 1;
-	    if (n) x += x;
-	    else break;
+	for ( i = 0; i < n; i++ ) {
+	    s += str.replace(rx, i);
 	}
 	return s;
     }
 
     function htmlTable(x, y) {
-	var t = "<table>"
-	t    += strrep("<column group>", y);
-	t    += strrep( "<tr>" + strrep("<td>X</td>", y) + "</tr>", x);
+	var t = "<table cellpadding=0 cellspacing=0>"
+	//t    += strrep("<column group>", y);
+	t      += strrep( "<tr>"
+		+ strrep("<td ><input type=entry size=7 name=cell%y.%x value=0></td>", x, "%x") 
+		+ "</tr>\n", y, "%y");
 	t    += "</table>";
+
+	console.log(t);
 
 	return t;
     }
@@ -42,20 +46,61 @@
 
   //   $(this).css({"font-size" : newFontSize, "line-height" : newFontSize/1.2 + "px"});
 
-    function pluginUpdate(im, xreg) {
-            var section = imexam.reg2section(xreg);
+    function pxtablUpdate(im, point) {
             var im_2d   = imexam.ndops.ndarray(im.raw.data, [im.raw.height, im.raw.width]);
-            var imag    = imexam.ndops.section(im_2d, section);
+	    var i = 0, j = 0;
+	    var x, y;
 
-	    $(this.div).html(htmlTable(10, 10));
+	    var pxtabl = $(this.div).find(".pxtabl")[0];
+
+	    pxtabl["cell" + j + "." + i].value = "col/row";
+
+	    j = 0
+	    for ( i = 1; i < 11; i++ ) {
+		x = point.x + i - 5
+
+		if ( x > 0 && x <= im.raw.width ) {
+		    pxtabl["cell" + j + "." + i].value = x;
+		} else {
+		    pxtabl["cell" + j + "." + i].value = "";
+		}
+	    }
+
+	    i = 0
+	    for ( j = 1; j < 11; j++ ) {
+		y = point.y + j - 5
+
+		if ( y > 0 && y <= im.raw.height ) {
+		    pxtabl["cell" + j + "." + i].value = y;
+		} else {
+		    pxtabl["cell" + j + "." + i].value = "";
+		}
+	    }
+
+	    for ( j = 1; j < 11; j++ ) {
+	    for ( i = 1; i < 11; i++ ) {
+		x = point.x + i - 5 - 1
+		y = point.y + j - 5 - 1
+
+		if ( x >= 0 && x < im.raw.width && y >= 0 && y < im.raw.height ) {
+		    pxtabl["cell" + j + "." + i].value = im_2d.get(y, x).toPrecision(5);
+		} else {
+		    pxtabl["cell" + j + "." + i].value = "";
+		}
+	    }
+	    }
+
+
     }
 
-    function pluginInit() {
+    function pxtablInit() {
 	imexam.fixupDiv(this);
+
+	$(this.div).html("<form class=pxtabl>" + htmlTable(10+1, 10+1) + "</form>");
     }
 
-    JS9.RegisterPlugin("ImExam", "PxTabl", pluginInit, {
-	    menu: "analysis",
+    JS9.RegisterPlugin("ImExam", "PxTabl", pxtablInit, {
+	    menu: "view",
 
             menuItem: "Pixel Table",
             winTitle: "Pixel Table",
@@ -64,8 +109,8 @@
 	    toolbarSeparate: true,
 	    toolbarHTML: " ",
 
-            regionchange: pluginUpdate,
-            winDims: [250, 250],
+            mousemove: pxtablUpdate,
+            winDims: [675, 275],
     });
 }());
 
